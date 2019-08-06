ab618dbe8e5df8b97a491fad7d19a247c56ad21e
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-06 16:36:38 -0300

    :sparkles: [ADD] Sequelize and config variables

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 
@@ -16,6 +17,17 @@ class App {
   routes() {
     this.server.use(routes);
   }
+
+  // exceptionHandler() {
+  //   this.server.use(async (err, req, res, next) => {
+  //     if (process.env.NODE_ENV === 'development') {
+  //       const errors = await new Youch(err, req).toJSON();
+
+  //       return res.status(500).json(errors);
+  //     }
+  //     return res.status(500).json({ error: 'Internal Server Error'});
+  //   })
+  // }
 }
 
 export default new App().server;
