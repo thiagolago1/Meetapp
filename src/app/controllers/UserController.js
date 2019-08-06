3193d8222783c5c676a8d1d41221e979bc6c4681
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-06 18:23:37 -0300

    :sparkles: [ADD] User Controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,24 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({
+        error:
+          'This User already exists. Please, if you forgot your password, try recovering it. If you are a new user, try again with different credentials.',
+      });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: '',
-    email: '',
-    password_hash: '',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
