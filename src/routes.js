b1f0a502dfc5882ff1551d0a95c9aace7e69b2d9
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-07 16:49:08 -0300

    :construction: [CREATE] Organizing Controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
+
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -13,8 +18,13 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
-routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+routes.put('/users', UserController.update);
+routes.get('/meetups', MeetupController.index);
+routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
+routes.get('/organizing', OrganizingController.index);
 
 export default routes;
