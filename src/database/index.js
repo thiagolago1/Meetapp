9a502fbd8aca72e8e978100c449677d2ba1b5eb0
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-07 16:09:16 -0300

    :construction: [CREATE] Models and Controllers

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,29 @@
 import Sequelize from 'sequelize';
-import User from '../app/models/User';
 import databaseConfig from '../config/database';
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Meetup from '../app/models/Meetup';
+import Subscription from '../app/models/Subscription';
 
-const models = [User];
+const models = [User, File, Meetup, Subscription];
 
 class Database {
   constructor() {
+    this.connection = new Sequelize(databaseConfig);
     this.init();
+    this.associate();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    models.forEach(model => model.init(this.connection));
+  }
 
-    models.map(model => model.init(this.connection));
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
   }
 }
 
