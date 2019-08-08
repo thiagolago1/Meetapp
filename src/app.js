872f70925344d13edd50ed1d27a1bda9ca6be534
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-08 17:50:58 -0300

    :sparkles: [ADD] express async errors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
 import sentryConfig from './config/sentry';
